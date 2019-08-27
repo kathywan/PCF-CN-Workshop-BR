cdfab16f83a8c2e485174f9910567481a724c542
Glenio Borges <gborges@dambor-mac.local>
2019-08-27 16:18:12 -0400

    create dirs

---
+++ labs/lab03/src/main/java/io/pivotal/cloudnativespring/GreetingController.java
@@ -0,0 +1,12 @@
+package io.pivotal.cloudnativespring;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
